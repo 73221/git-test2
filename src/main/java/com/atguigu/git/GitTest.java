332ec8adff74c89c6fe942583007b71b89c83ac6
Dyf <Dyf@atguigu.com>
2023-02-22 09:02:37 +0800

    first-commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 21
+ * @version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
